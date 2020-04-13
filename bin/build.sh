beb2c77902c2d5049a318c454fcbbbdf4ef6df5b
Miwoy <miwoes@163.com>
2020-04-13 19:34:13 +0800

    update node@v12.x

--- bin/build.sh
+++ bin/build.sh
@@ -1,11 +1,8 @@
 rm -rf build
 mkdir build
-cp package.json build/package
-cp -rf bin build/bin
-cp -rf src build/src
-cp -rf views build/views
-babel app.js -o build/app.js
-babel application -d build/application
-babel lib -d build/lib
-babel models -d build/models
-babel routes -d build/routes
+
+npx babel ./ -d build/ --only "application/*,lib/*,models/*,routes/*,views/*,app.js"
+npx babel bin/ -d build/bin/ --copy-files
+npx babel src/ -d build/src/ --copy-files
+cp ./README.md build/ 
+cp ./package.json build/ 
